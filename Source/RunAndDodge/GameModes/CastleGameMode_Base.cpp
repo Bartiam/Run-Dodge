1ad984d35614c088993b534cac0fb04f6e817940
Bartiam <leswar@yandex.ru>
2024-11-03 18:35:44 +0500

    Updates.

---
+++ Source/RunAndDodge/GameModes/CastleGameMode_Base.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "../GameModes/CastleGameMode_Base.h"
+
---
+++ Source/RunAndDodge/Types/Types_Base.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Types_Base.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RUNANDDODGE_API UTypes_Base : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
